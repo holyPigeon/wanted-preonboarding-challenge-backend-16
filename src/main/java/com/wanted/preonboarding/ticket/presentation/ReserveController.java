ff9a290bb5d6383005607dada64fb6e6f738b16a
jae woo Jung <awfjol2008@naver.com>
2024-01-23 22:05:06 +0900

    feat: 공연 및 전시 예약 기능 구현

--- src/main/java/com/wanted/preonboarding/ticket/presentation/ReserveController.java
+++ src/main/java/com/wanted/preonboarding/ticket/presentation/ReserveController.java
@@ -3,9 +3,10 @@ package com.wanted.preonboarding.ticket.presentation;
 import com.wanted.preonboarding.ticket.application.TicketSeller;
 import com.wanted.preonboarding.ticket.domain.dto.response.ReserveInfo;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/reserve")
@@ -14,18 +15,18 @@ public class ReserveController {
     private final TicketSeller ticketSeller;
 
     @PostMapping("/")
-    public boolean reservation() {
+    public boolean reservation(@RequestBody ReserveInfo reserveInfo) {
         System.out.println("reservation");
 
         return ticketSeller.reserve(ReserveInfo.builder()
-            .performanceId(UUID.fromString("4438a3e6-b01c-11ee-9426-0242ac180002"))
-            .reservationName("유진호")
-            .reservationPhoneNumber("010-1234-1234")
-            .reservationStatus("reserve")
-            .amount(200000)
-            .round(1)
-            .line('A')
-            .seat(1)
+            .performanceId(reserveInfo.getPerformanceId())
+            .reservationName(reserveInfo.getReservationName())
+            .reservationPhoneNumber(reserveInfo.getReservationPhoneNumber())
+            .reservationStatus(reserveInfo.getReservationStatus())
+            .amount(reserveInfo.getAmount())
+            .round(reserveInfo.getRound())
+            .line(reserveInfo.getLine())
+            .seat(reserveInfo.getSeat())
             .build()
         );
     }
