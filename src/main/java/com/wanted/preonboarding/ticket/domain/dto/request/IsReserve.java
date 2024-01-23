e456c750277e23d46a2e1c3d8a4d29fa5bdfa843
jae woo Jung <awfjol2008@naver.com>
2024-01-23 20:20:06 +0900

    feat: 공연 및 전시 정보 조회 기능 관련 request dto인 IsReserve 구현

---
+++ src/main/java/com/wanted/preonboarding/ticket/domain/dto/request/IsReserve.java
@@ -0,0 +1,12 @@
+package com.wanted.preonboarding.ticket.domain.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class IsReserve {
+    private String isReserve;
+}
