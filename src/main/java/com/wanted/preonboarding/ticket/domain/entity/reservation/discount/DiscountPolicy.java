13e161fba327e72b3d908366616151a9057053b1
jae woo Jung <awfjol2008@naver.com>
2024-01-26 01:10:17 +0900

    feat: 할인 정책 인터페이스에 할인 코드 및 이름 관련 getter 추가

--- src/main/java/com/wanted/preonboarding/ticket/domain/entity/reservation/discount/DiscountPolicy.java
+++ src/main/java/com/wanted/preonboarding/ticket/domain/entity/reservation/discount/DiscountPolicy.java
@@ -2,4 +2,6 @@ package com.wanted.preonboarding.ticket.domain.entity.reservation.discount;
 
 public interface DiscountPolicy {
     public int discount(int price);
+    public int getCode();
+    public String getName();
 }
