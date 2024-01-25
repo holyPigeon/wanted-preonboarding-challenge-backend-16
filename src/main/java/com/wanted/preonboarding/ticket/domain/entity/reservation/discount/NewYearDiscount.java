bfdfc49691d0bf95b30afc200e7aa255c77c40e1
jae woo Jung <awfjol2008@naver.com>
2024-01-26 01:10:33 +0900

    feat: 새해 할인 정책에 인터페이스 변경 사항 적용

--- src/main/java/com/wanted/preonboarding/ticket/domain/entity/reservation/discount/NewYearDiscount.java
+++ src/main/java/com/wanted/preonboarding/ticket/domain/entity/reservation/discount/NewYearDiscount.java
@@ -1,8 +1,21 @@
 package com.wanted.preonboarding.ticket.domain.entity.reservation.discount;
 
 public class NewYearDiscount implements DiscountPolicy {
+    private final String name = "NEW_YEAR";
+    private final int code = 1;
+
     @Override
     public int discount(int price) {
         return (int) (price * 0.8);
+    }
+
+    @Override
+    public int getCode() {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return name;
     }
 }
