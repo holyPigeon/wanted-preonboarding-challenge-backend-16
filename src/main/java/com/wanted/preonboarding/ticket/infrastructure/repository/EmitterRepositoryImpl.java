ab00d3adad6a2dec879ded199228eb35bb7db6e2
jae woo Jung <awfjol2008@naver.com>
2024-01-27 20:30:45 +0900

    feat: SSE Emitter 관련 리포지토리 인터페이스 구현체 구현

---
+++ src/main/java/com/wanted/preonboarding/ticket/infrastructure/repository/EmitterRepositoryImpl.java
@@ -0,0 +1,44 @@
+package com.wanted.preonboarding.ticket.infrastructure.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+@Repository
+@RequiredArgsConstructor
+public class EmitterRepositoryImpl implements EmitterRepository {
+    private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
+    private final Map<String, Object> eventCache = new ConcurrentHashMap<>();
+
+    @Override
+    public SseEmitter save(String id, SseEmitter sseEmitter) {
+        emitters.put(id, sseEmitter);
+        return sseEmitter;
+    }
+
+    @Override
+    public void saveEventCache(String id, Object event) {
+        eventCache.put(id, event);
+    }
+
+    @Override
+    public Map<String, SseEmitter> findAllStartById(String id) {
+        return emitters.entrySet().stream()
+                .filter(entry -> entry.getKey().startsWith(id))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    @Override
+    public void deleteAllStartWithId(String id) {
+        emitters.forEach((key, value) -> {
+            if (key.startsWith(id)) {
+                emitters.remove(key);
+            }
+        });
+    }
+
+}
